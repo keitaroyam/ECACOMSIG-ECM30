5886db87f2b4ae051967fdf5d3e010720fac12ce
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2016-08-13 14:35:51 +0900

    Add first version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ECACOMSIG-ECM30
-My tutorial materials for ECACOMSIG computing school (August 2016)
+# Playing with XDS predictions (using Numpy)
+This is a tutorial at [ECACOMSIG computing school (Freudenstadt, August 2016)](http://www.mrc-lmb.cam.ac.uk/harry/ecacomsig/freudenstadt.html)
+
+In this tutorial we will implement calculation of spot positions in 3D space (x,y,phi) from [XPARM.XDS](http://xds.mpimf-heidelberg.mpg.de/html_doc/xds_files.html#XPARM.XDS).
+
+Reference paper: [W. Kabsch (2010) "Integration, scaling, space-group assignment and post-refinement" Acta Cryst. (2010). D66, 133-144](http://dx.doi.org/10.1107/S0907444909047374)
+
+Test data: [Thaumatin / Diamond Light Source I04 user training](https://zenodo.org/record/10271#.V6vChZOLRBz)
+
+Before the tutorial, you need to install
+* Python2.7 with [CCTBX](http://cctbx.sourceforge.net/) and [Numpy](http://www.numpy.org/) (1.8 or higher)
+   * You can use phenix.python if you installed [phenix-1.10.1](https://www.phenix-online.org/)
+* [XDS and generate_XDS.INP](http://strucbio.biologie.uni-konstanz.de/xdswiki/index.php/Installation)
+* [Adxv](http://www.scripps.edu/tainer/arvai/adxv.html)
---
+++ prediction.py
@@ -0,0 +1,169 @@
+"""
+(c) RIKEN 2016. All rights reserved. 
+Author: Keitaro Yamashita
+
+This software is released under the new BSD License.
+"""
+
+import numpy
+from cctbx import miller
+from cctbx import crystal
+
+class Predictions:
+    def __init__(self):
+        pass
+
+    def read_xparm(self, xpin):
+        fin = open(xpin)
+        assert "XPARM.XDS" in fin.readline() # Skip first header line
+
+        # Line 2
+        sp = fin.readline().split() # starting frame & angle, osc. range, rotation axis
+        self.starting_frame, self.starting_angle, self.osc_range = int(sp[0]), float(sp[1]), float(sp[2])
+        m2 = numpy.array(map(float, sp[3:6]))
+
+        # Line 3
+        sp = fin.readline().split() # wavelength & incident beam direction
+        self.wavelength = float(sp[0])
+        incident_beam = numpy.array(map(float, sp[1:4]))
+        self.s0 = incident_beam / numpy.linalg.norm(incident_beam) / self.wavelength
+
+        m = numpy.empty(dtype=numpy.float, shape=(3,3)) # (m1 m2 m3) matrix
+        m[:,1] = m2 / numpy.linalg.norm(m2)
+        m[:,0] = numpy.cross(m[:,1], self.s0) # m1 = m2 x s0 / |m2 x s0|
+        m[:,0] /= numpy.linalg.norm(m[:,0])
+        m[:,2] = numpy.cross(m[:,0], m[:,1]) # m3 = m1 x m2
+        self.m_matrix = m
+        
+        # Line 4
+        sp = fin.readline().split() # Space group number & unit cell constants
+        self.crystal_symmetry = crystal.symmetry(unit_cell=map(float, sp[1:7]),
+                                                 space_group_symbol=int(sp[0]))
+
+        # Line 5,6,7 real space vectors
+        a_axis = map(float, fin.readline().split())
+        b_axis = map(float, fin.readline().split())
+        c_axis = map(float, fin.readline().split())
+        
+        b_mat = numpy.array([a_axis, b_axis, c_axis]).transpose() # (b0 b1 b2) matrix
+        self.astar_matrix = numpy.linalg.inv(b_mat) # (b0* b1* b2*)^t matrix
+
+        # Line 8 detector dimensions and pixel size
+        sp = fin.readline().split()
+        self.nxy = map(int, sp[1:3]) # NX, NY
+        self.qxy = map(float, sp[3:5]) # QX, QY
+
+        # Line 9 ORGX,Y & detector distance
+        sp = fin.readline().split()
+        self.orgxy = map(float, sp[:2])
+        self.detector_F = float(sp[2])
+        
+        # Line 10,11,12
+        d1 = map(float, fin.readline().split())
+        d2 = map(float, fin.readline().split())
+        d3 = map(float, fin.readline().split()) # this is actually d1 x d2
+
+        self.d_matrix = numpy.array([d1,d2,d3]).transpose()  # (d1 d2 d3) matrix
+    # read_xparm()
+
+    def prep_indices(self, d_min, d_max=None):
+        mset = miller.build_set(self.crystal_symmetry, anomalous_flag=True, d_min=d_min, d_max=d_max)
+        return mset.indices()
+    # prep_indices()
+
+    def calc_centroids(self, indices):
+        h = numpy.array(indices) # hkl in each row
+        m, d, a, F = self.m_matrix, self.d_matrix, self.astar_matrix, self.detector_F
+        orgx, orgy = self.orgxy
+        qx, qy = self.qxy
+        s0 = self.s0
+
+        p0s = numpy.dot(h, a) # p0* vector in each row
+        p0s_m = numpy.dot(p0s, m) # p0* with m1,m2,m3 basis
+
+        s0_m = numpy.dot(m.transpose(), s0) # s0 with m1,m2,m3 basis
+
+        p0s_lensq = numpy.sum(p0s**2, axis=1) # |p0*|^2 = |p*|^2
+
+        ps_m = numpy.empty(p0s_m.shape) # p* with m1,m2,m3 basis
+        ps_m[:,2] = (-0.5*p0s_lensq - s0_m[1]*p0s_m[:,1]) / s0_m[2]
+        ps_m[:,1] = p0s_m[:,1]
+        ps_m[:,0] = p0s_lensq - ps_m[:,1]**2 - ps_m[:,2]**2 # sqrt after check
+
+        sel_ok = ps_m[:,0] > 0
+        
+        h, p0s_m, ps_m = h[sel_ok], p0s_m[sel_ok], ps_m[sel_ok]
+        ps_m[:,0] = numpy.sqrt(ps_m[:,0])
+        
+        get_phi = lambda x1,x3: numpy.arctan2(p0s_m[:,2]*x1 - p0s_m[:,0]*x3, # sin(phi)
+                                              p0s_m[:,0]*x1 + p0s_m[:,2]*x3) # cos(phi)
+
+        self.predicted_hkl = numpy.empty((0, 3), dtype=numpy.int) # h,k,l
+        self.predicted_data = numpy.empty((0, 4)) # x, y, phi, zeta
+
+        for p1sign in (+1, -1):
+            ps_m[:,0] *= p1sign
+            phi = get_phi(ps_m[:,0], ps_m[:,2])
+            e1 = numpy.cross(ps_m, s0_m)
+            e1 /= numpy.linalg.norm(e1, axis=1)[:,numpy.newaxis]
+            zeta = numpy.abs(e1[:,1]) # as e1 is expressed in m1,m2,m3 system
+
+            s = s0 + numpy.dot(ps_m, m.transpose())
+            s_d = numpy.dot(s, d) # S vector with d1,d2,d3 basis
+            sel_ok = F*s_d[:,2] > 0
+            s_d, h_ok, phi, zeta = s_d[sel_ok], h[sel_ok], phi[sel_ok], zeta[sel_ok]
+            xdet = orgx + F*s_d[:,0]/s_d[:,2]/qx
+            ydet = orgy + F*s_d[:,1]/s_d[:,2]/qy
+
+            self.predicted_hkl = numpy.row_stack([self.predicted_hkl, h_ok])
+            self.predicted_data = numpy.row_stack([self.predicted_data, numpy.column_stack([xdet, ydet, phi, zeta])])
+
+    # calc_centroids()
+
+    def get_predicted_positions(self, sigmar, frame, esd_factor=3.5):
+        phi = self.starting_angle + self.osc_range * (frame - self.starting_frame + 0.5) # Is this correct?
+        print "  Phi at frame %d = %.3f" % (frame, phi)
+        
+        phi, sigmar = numpy.deg2rad([phi, sigmar])
+
+        phi_calc = self.predicted_data[:,2]
+        zeta = self.predicted_data[:,3]
+
+        phi_diff = numpy.fmod(phi_calc - phi, 2.*numpy.pi)
+        phi_diff[phi_diff < -numpy.pi] += 2.*numpy.pi
+        phi_diff[phi_diff > numpy.pi] -= 2.*numpy.pi
+
+        sel = numpy.abs(phi_diff) < esd_factor * sigmar / zeta
+        return self.predicted_hkl[sel], self.predicted_data[sel]
+    # get_predicted_positions()
+
+# class Predictions
+
+if __name__ == "__main__":
+    import sys
+
+    xparm_in = sys.argv[1]
+    d_min = float(sys.argv[2])
+    sigma_r = float(sys.argv[3])
+    frames = map(int, sys.argv[4:])
+
+    
+    preds = Predictions()
+    print "Reading XPARM.XDS.."
+    preds.read_xparm(xparm_in)
+
+    indices = preds.prep_indices(d_min)
+
+    print "Calculating the predicted centroids.."
+    preds.calc_centroids(indices)
+    
+    for frame in frames:
+        print "Calculating the predictions on frame %d.." % frame
+        pindices, pdata = preds.get_predicted_positions(sigma_r, frame)
+
+        ofs = open("prediction_%.6d.adx" % frame, "w")
+        for (h,k,l), (x, y, phi, zeta) in zip(pindices, pdata):
+            ofs.write("%d %d %d %d %d\n"%(x,y, h,k,l))
+
+        ofs.close()
+        
